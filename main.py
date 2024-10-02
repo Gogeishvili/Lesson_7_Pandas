ed7501cee112f2727e2cec4d96bafbe4e4aea9fe
Gogeishvili <gio.gogeishvili@gmail.com>
2024-10-02 20:45:40 +0400

    Init

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+data = {'Name': ['Alica', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New Your', 'Los Angeles', 'Chicago']}
+
+df=pd.DataFrame(data)
+print(df)
